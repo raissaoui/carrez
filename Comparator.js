43e62ad6d7700f35216ba1630c6d2d2dd532ab76
ramez aissaoui <ramez aissaoui>
2017-02-10 17:25:34 +0100

    modification de comparator.js

--- Comparator.js
+++ Comparator.js
@@ -1,4 +1,4 @@
-/*formulaire*/
+/*formulaire
 <FORM>
     <INPUT type="text" name="Ville" value="Valeur initiale">
     <INPUT type="text" name="Prix" value="Valeur initiale">
@@ -9,3 +9,4 @@
     <INPUT type="button" value="Changer le contenu"
     onClick=' this.form.zonedetexte.value="NOUVEAU" '>
 </FORM>
+*/
