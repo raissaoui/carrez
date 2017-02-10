519ef568a03faa8ba0f1ad29ae3c4b6580f6083b
ramez aissaoui <ramez aissaoui>
2017-02-10 16:25:22 +0100

    copy selector prix d'un apparte+maison

--- MeilleursAgents.js
+++ MeilleursAgents.js
@@ -6,30 +6,32 @@ var app     = express();
 
 app.get('/scrapeMA', function(req, res){
   var title;
-    url = 'https://www.meilleursagents.com/prix-immobilier/#estimates';
+    url = 'http://www.meilleursagents.com/prix-immobilier/';
 
     request(url, function(error, response, html){
         if(!error){
             var $ = cheerio.load(html);
+            var ville="";
+            var cp="";
             var lowest_price_House;
             var lowest_price_Flat;
             var mean_price_House;
             var mean_price_Flat;
             var highest_price_House;
             var highest_price_Flat;
-            var json_House = { lowest_price_House: "", mean_price_House: "", highest_price_House: ""};
-            var json_Flat = {lowest_price_Flat: "",  mean_price_Flat: "", highest_price_Flat: ""};
+            var json_House = { lowest_price_House: "", medium_price_House: "", highest_price_House: ""};
+            var json_Flat = {lowest_price_Flat: "",  medium_price_Flat: "", highest_price_Flat: ""};
 
             $('.row medium-uncollapse baseline--half').find('div.small-12 medium-6 columns prices-summary__cell--row-header > div.left').each(function(i, elm){
               if($(this).text()=="Prix m2 appartement"){
-                json_Flat.lowest_price_Flat=$(this).parents().parents().find().text();
-                json_Flat.mean_price_Flat=$(this).parents().parents().find().text();
-                json_Flat.highest_price_Flat=$(this).parents().parents().find().text();
+                json_Flat.lowest_price_Flat=$(this).parents().parents().find('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').text();
+                json_Flat.medium_price_Flat=$(this).parents().parents().find('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.columns.prices-summary__cell--median').text();
+                json_Flat.highest_price_Flat=$(this).parents().parents().find('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div:nth-child(4)').text();
               }
               if($(this).text()=="Prix m2 maison"){
-                json_House.lowest_price_House=$(this).parents().parents().find().text();
-                json_House.mean_price_House=$(this).parents().parents().find().text();
-                json_House.highest_price_House=$(this).parents().parents().find().text();
+                json_House.lowest_price_House=$(this).parents().parents().find('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').text();
+                json_House.medium_price_House=$(this).parents().parents().find('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.columns.prices-summary__cell--median').text();
+                json_House.highest_price_House=$(this).parents().parents().find('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div:nth-child(4)').text();
               }
             })
 
