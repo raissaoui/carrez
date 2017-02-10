9f462d78a277e595719da9869b02c7b4c39a1cfd
ramez aissaoui <ramez aissaoui>
2017-02-10 19:05:35 +0100

    main.js

--- Main.js
+++ Main.js
@@ -6,7 +6,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-/*function include(fileName){
+function include(fileName){
 
   document.write("<script type='text/javascript' src='"+fileName+"'></script>" );
 
@@ -16,10 +16,40 @@ function cleanData(text){
   text.replace(/(\r\n|\n|\r)/gm,"");
   text=text.trim();
   return text;
-}*/
+}
+var price_annonce;
+var type;
+var lowest_price_House;
+var lowest_price_Flat;
+var highest_price_Flat;
+var highest_price_House;
+var medium_price_Flat;
+var medium_price_House;
+var message;
 
 app.get('/main', function(req, res){
   res.send('home');
+  
+  if (type=="House"){
+    if(price>medium_price_House){
+      message="Bad deal";
+      console.log(message);
+    }
+    else {
+      message="Good deal";
+      console.log(message);
+    }
+  }
+  else if(type=="Flat"){
+    if(price>medium_price_Flat){
+      message="Bad deal";
+      console.log(message);
+    }
+    else{
+      message="Good deal";
+      console.log(message);
+    }
+  }
 })
 
 app.listen('3000')
