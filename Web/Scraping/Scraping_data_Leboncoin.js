1b4c4b2b76787c06bd7d81e9362ba97648970419
ramez aissaoui <ramez aissaoui>
2017-02-22 22:39:57 +0100

    changement du scraping

--- Web/Scraping/Scraping_data_Leboncoin.js
+++ Web/Scraping/Scraping_data_Leboncoin.js
@@ -1,11 +1,8 @@
-vvar express = require('express');
+var express = require('express');
 var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-
-
-
 
 
 function cleanData(text){
@@ -14,93 +11,71 @@ function cleanData(text){
   return text;
 }
 
-var leboncoin = function(url,callback){
-//app.get('/scrapingLeboncoin', function(req, res){
-  var json = {price : "", city : "", type : "", surface:""};
-  var url = 'https://www.leboncoin.fr/ventes_immobilieres/1089637884.htm?ca=12_s';
+var url = 'https://www.leboncoin.fr/ventes_immobilieres/1089637884.htm?ca=12_s';
+var price;
+var city;
+var type;
+var surface;
 
+var leboncoin_price = function(url,callback){
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+        $('#adview > section > section > section.properties.lineNegative > div:nth-child(5) > h2 > span.value').filter(function(){
+          var data=$(this).text();
 
+          price =data;
 
+          console.log(price);
+        })
+    }
+    callback(price);
+  })
+}
+var leboncoin_city = function(url,callback){
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+        $('#adview > section > section > section.properties.lineNegative > div.line.line_city').filter(function(){
+          var data=$(this).text();
+          city=data;
+          console.log(city);
+        })
+    callback(city);
+  }
+})
+}
+var leboncoin_type = function(url,callback){
   request(url, function(error, response, html){
     if(!error){
       var $ = cheerio.load(html);
 
-
-        $('#adview > section > section > section.properties.lineNegative > div:nth-child(5) > h2 > span.value').filter(function(){
-          var data=$(this).text();
-
-          json.price =data;
-
-          console.log(json.price);
-        })
-
-        $('#adview > section > section > section.properties.lineNegative > div.line.line_city').filter(function(){
-          var data=$(this).text();
-
-          json.city=data;
-
-          console.log(json.city);
-        })
-
         $('#adview > section > section > section.properties.lineNegative > div:nth-child(7)').filter(function(){
           var data=$(this).text();
 
-          json.type=data;
+          type=data;
 
-          console.log(json.type);
+          console.log(type);
         })
-
+    }
+    callback(type);
+  })
+}
+var leboncoin_surface= function(url,callback){
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
         $('#adview > section > section > section.properties.lineNegative > div:nth-child(9)').filter(function(){
           var data=$(this).text();
-
-          json.surface=data;
-
-          console.log(json.surface);
+          surface=data;
+          console.log(surface);
         })
-
-
-
-      // get data
-      $('.properties.lineNegative').find('div.line > h2 > span.value').each(function(i, elm) {
-          var data=$(this);
-          var prop=data.prev();
-          if(prop.text()=="Type de bien"){
-              json.type=data.text();
-          }
-          else if (prop.children().first().text()=="Ville") {
-            json.city=cleanData(data.text());
-          }
-          else if (prop.text()=="Surface") {
-            json.surface=data.text();
-          }
-      });
-
-      $('.ratingValue').filter(function(){
-        var data = $(this);
-        rating = data.text().trim();
-
-        json.rating = rating;
-      })
     }
-
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-      console.log('File successfully written! - Check your project directory for the output.json file');
-    })
-
-    //res.send('Check your console!')
-    callback(json);
+    callback(surface);
   })
 }
 
-/*function compute(url,callback){
-  var json=lbc(url);
-  callback && callback(json.town,json.price);
-}
-compute(url,function(town,price));*/
-
-/*app.listen('3000')
-console.log('Magic happens on port 3000');
-exports = module.exports = app;*/
-
-
-exports.leboncoin = leboncoin;
+exports.leboncoin_price = leboncoin_price;
+exports.leboncoin_city = leboncoin_city;
+exports.leboncoin_type = leboncoin_type;
+exports.leboncoin_surface = leboncoin_surface;
--- Web/Scraping/Scraping_data_MeilleursAgents.js
+++ Web/Scraping/Scraping_data_MeilleursAgents.js
@@ -5,93 +5,118 @@ var cheerio = require('cheerio');
 var app     = express();
 var ville="";
 var cp="";
+var url='http://www.meilleursagents.com/prix-immobilier/courbevoie-92400/';
+var lowest_price_House;
+var lowest_price_Flat;
+var medium_price_House;
+var medium_price_Flat;
+var highest_price_House;
+var highest_price_Flat;
 
 
-var MeilleursAgents = function(url,callback){
-
-//app.get('/scrapingMeilleursAgents', function(req, res){
-  var title;
-    //url = 'http://www.meilleursagents.com/prix-immobilier/'+ville+'-'+cp+'/';
-    url='http://www.meilleursagents.com/prix-immobilier/courbevoie-92400/';
-
-    request(url, function(error, response, html){
+var MeilleursAgents_lowest_price_Flat = function(url,callback){
+  request(url, function(error, response, html){
         if(!error){
             var $ = cheerio.load(html);
-            var lowest_price_House;
-            var lowest_price_Flat;
-            var medium_price_House;
-            var medium_price_Flat;
-            var highest_price_House;
-            var highest_price_Flat;
-            var json_House = { lowest_price_House: "", medium_price_House: "", highest_price_House: ""};
-            var json_Flat = {lowest_price_Flat: "",  medium_price_Flat: "", highest_price_Flat: ""};
-
             $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').filter(function(){
               var data=$(this).text();
 
-              json_Flat.lowest_price_Flat=data;
+              lowest_price_Flat=data;
 
-              console.log(json_Flat.lowest_price_Flat);
-            })
-
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
-              var data=$(this).text();
-
-              json_Flat.medium_price_Flat=data;
-
-              console.log(json_Flat.medium_price_Flat);
-            })
-
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div:nth-child(4)').filter(function(){
-              var data=$(this).text();
-
-              json_Flat.highest_price_Flat=data;
-
-              console.log(json_Flat.highest_price_Flat);
-            })
-
-
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').filter(function(){
-              var data=$(this).text();
-
-              json_House.lowest_price_House=data;
-
-              console.log(json_House.lowest_price_House);
-            })
-
-
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
-              var data=$(this).text();
-
-              json_House.medium_price_Flat=data;
-
-              console.log(json_House.medium_price_Flat);
-            })
-
-
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div:nth-child(4)').filter(function(){
-              var data=$(this).text();
-
-              json_House.highest_price_House=data;
-
-              console.log(json_House.highest_price_House);
-            })
-
-
-            fs.writeFile('meilleursagents.js', JSON.stringify(json_House,json_Flat, 6), function(err){
-                res.send('File successfully written! - Check your project directory for the output.json file');
+              console.log(lowest_price_Flat);
             })
         }
-        res.send('Check your console!')
-        callback(json_House);
-        callback(json_Flat);
+        callback(lowest_price_Flat);
     })
 
 }
 
+var MeilleursAgents_medium_price_Flat = function(url,callback){
+  request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
+              var data=$(this).text();
 
+              medium_price_Flat=data;
 
-/*app.listen('3000')
-console.log('Magic happens on port 3000');*/
-//exports = module.exports = app;
-exports.MeilleursAgents = MeilleursAgents;
+              console.log(medium_price_Flat);
+            })
+        }
+        callback(medium_price_Flat);
+    })
+}
+
+var MeilleursAgents_highest_price_Flat = function(url,callback){
+  request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div:nth-child(4)').filter(function(){
+              var data=$(this).text();
+
+              highest_price_Flat=data;
+
+              console.log(highest_price_Flat);
+            })
+        }
+        callback(highest_price_Flat);
+    })
+
+}
+
+var MeilleursAgents_lowest_price_House = function(url,callback){
+  request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').filter(function(){
+              var data=$(this).text();
+
+              lowest_price_House=data;
+
+              console.log(lowest_price_House);
+            })
+        }
+        callback(lowest_price_House);
+    })
+
+}
+
+var MeilleursAgents_medium_price_Flat = function(url,callback){
+  request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
+              var data=$(this).text();
+
+              medium_price_Flat=data;
+
+              console.log(medium_price_Flat);
+            })
+        }
+        callback(medium_price_Flat);
+    })
+
+}
+
+var MeilleursAgents_highest_price_House = function(url,callback){
+  request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div:nth-child(4)').filter(function(){
+              var data=$(this).text();
+
+              highest_price_House=data;
+
+              console.log(highest_price_House);
+            })
+        }
+        callback(highest_price_House);
+    })
+}
+
+exports.MeilleursAgents_lowest_price_Flat = MeilleursAgents_lowest_price_Flat;
+exports.MeilleursAgents_medium_price_Flat = MeilleursAgents_medium_price_Flat;
+exports.MeilleursAgents_highest_price_Flat = MeilleursAgents_highest_price_Flat;
+exports.MeilleursAgents_lowest_price_House = MeilleursAgents_lowest_price_House;
+exports.MeilleursAgents_medium_price_Flat = MeilleursAgents_medium_price_Flat;
+exports.MeilleursAgents_highest_price_House = MeilleursAgents_highest_price_House;
